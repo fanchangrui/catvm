9ed878abc31cc67c4cb2b4b035ed887bee45c467
fanchangrui <1293707902@qq.com>
2022-08-22 14:34:57 +0800

    默认开启代理

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const catvm2code = catvm2({proxy:false});
+const catvm2code = catvm2({proxy:true});
 const codefile = `${__dirname}/code.js`;
 const vm = new VM()
 const script = new VMScript(catvm2code + fs.readFileSync(codefile),`${__dirname}/调试.js`);
